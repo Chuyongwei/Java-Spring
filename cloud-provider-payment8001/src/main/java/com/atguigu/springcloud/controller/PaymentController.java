7e4ac61f6619c383b6f2a2530b6ef03f5e839b7b
chuyongwei <1429303705@qq.com>
2020-07-27 23:06:27 +0800

    Some simple configurations of Eureka

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -14,12 +15,15 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("********插入结果："+result);
         if(result>0){
-            return new CommonResult(200,"插入数据库成功",result);
+            return new CommonResult(200,"插入数据库成功,serverPort:"+serverPort,result);
         }else {
             return new CommonResult(444,"插入失败",null);
         }
@@ -32,9 +36,9 @@ public class PaymentController {
         log.info("********插入结果："+payment);
 
         if(payment!=null){
-            return new CommonResult(200,"查询成2功",payment);
+            return new CommonResult(200,"查询成功,serveProt:"+serverPort,payment);
         }else {
-            return new CommonResult(444,"插入失败",null);
+            return new CommonResult(444,"没有对应记录，查询ID："+id,null);
         }
     }
 }
